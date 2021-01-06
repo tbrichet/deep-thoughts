8e1eaa7aafda216f58114929eb5e0f98992cf142
T Brichetto <TaraPersonal@MacBook-Air.local>
2021-01-05 22:15:47 -0700

    Lesson 21.2

--- server/server.js
+++ server/server.js
@@ -6,12 +6,16 @@ const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
+// import middleware for JWS authentication
+const { authMiddleware } = require('./utils/auth');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
